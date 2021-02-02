06e8d4654f5989a15522402b530b0d66c9f59dfd
niff <nifferdil@gmail.com>
2021-02-02 14:56:55 -0800

    test

--- app/src/main/java/com/example/samplegithubapp/MainActivity.kt
+++ app/src/main/java/com/example/samplegithubapp/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    // test
+    // testing
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
